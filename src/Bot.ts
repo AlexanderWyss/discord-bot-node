931ea72cd681e37565c8d31d51f21f110a654976
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-03-19 22:33:40 +0100

    activity

--- src/Bot.ts
+++ src/Bot.ts
@@ -13,91 +13,94 @@ import {GuildMusicManager} from "./music/GuildMusicManager";
 
 export class Bot {
 
-  public get client(): CommandoClient {
-    return this.commandoClient;
-  }
-
-  public static getInstance(): Bot {
-    if (!this.bot) {
-      this.bot = new Bot();
-      const token = process.env.TOKEN;
-      const owner = process.env.OWNER;
-      this.bot.start(token, owner);
+    public get client(): CommandoClient {
+        return this.commandoClient;
     }
-    return this.bot;
-  }
 
-  public static start() {
-    this.getInstance();
-  }
-
-  private static bot: Bot;
-
-  private commandoClient: CommandoClient;
-  private musicManagers = new Map<Snowflake, GuildMusicManager>();
-
-  private constructor() {
-
-  }
-
-  public start(token: string, owner: string) {
-    this.commandoClient = new CommandoClient({owner, commandPrefix: "!"});
-    this.commandoClient.registry
-      .registerGroups([
-        ["music", "Music"]
-      ])
-      .registerDefaultGroups()
-      .registerDefaultTypes()
-      .registerDefaultCommands({ping: false})
-      .registerCommands([
-        new PlayCommand(this), new JoinCommand(this), new LeaveCommand(this),
-        new SkipCommand(this), new PauseCommand(this), new ResumeCommand(this),
-        new MusicPanelCommand(this), new VolumeCommand(this), new PlayerCommand(this)
-      ]);
-    this.commandoClient.login(token).then(success => console.log("Logged in"), error => console.log("Login failed: " + error));
-    process.on("exit", () => {
-      this.close();
-    });
-    process.on("SIGINT", () => {
-      this.close();
-      process.exit();
-    });
-  }
-
-  public getGuildMusicManager(guild: Guild): GuildMusicManager {
-    if (this.musicManagers.has(guild.id)) {
-      return this.musicManagers.get(guild.id);
+    public static getInstance(): Bot {
+        if (!this.bot) {
+            this.bot = new Bot();
+            const token = process.env.TOKEN;
+            const owner = process.env.OWNER;
+            this.bot.start(token, owner);
+        }
+        return this.bot;
     }
-    const musicManager = new GuildMusicManager(guild);
-    this.musicManagers.set(guild.id, musicManager);
-    return musicManager;
-  }
 
-  public getGuildMusicManagerById(guildId: Snowflake): GuildMusicManager {
-    const guild = this.client.guilds.resolve(guildId);
-    if (guild) {
-      return this.getGuildMusicManager(guild);
-    } else {
-      throw new Error("Guild not available.");
+    public static start() {
+        this.getInstance();
     }
-  }
 
-  public getGuildMusicManagerByIdIfExists(guildId: Snowflake): GuildMusicManager {
-    if (this.musicManagers.has(guildId)) {
-      return this.musicManagers.get(guildId);
-    } else {
-      throw new Error("Guild not available.");
+    private static bot: Bot;
+
+    private commandoClient: CommandoClient;
+    private musicManagers = new Map<Snowflake, GuildMusicManager>();
+
+    private constructor() {
+
     }
-  }
 
-  private close() {
-    this.closeMusicManagers();
-    this.client.destroy();
-  }
-
-  private closeMusicManagers() {
-    for (const musicManager of this.musicManagers.values()) {
-      musicManager.close();
+    public start(token: string, owner: string) {
+        this.commandoClient = new CommandoClient({owner, commandPrefix: "!"});
+        this.commandoClient.registry
+            .registerGroups([
+                ["music", "Music"]
+            ])
+            .registerDefaultGroups()
+            .registerDefaultTypes()
+            .registerDefaultCommands({ping: false})
+            .registerCommands([
+                new PlayCommand(this), new JoinCommand(this), new LeaveCommand(this),
+                new SkipCommand(this), new PauseCommand(this), new ResumeCommand(this),
+                new MusicPanelCommand(this), new VolumeCommand(this), new PlayerCommand(this)
+            ]);
+        this.commandoClient.login(token).then(success => {
+            this.commandoClient.user.setActivity(this.commandoClient.commandPrefix + "Help").catch(err => console.error("Set Activity failed: " + err));
+            console.log("Logged in");
+        }, error => console.log("Login failed: " + error));
+        process.on("exit", () => {
+            this.close();
+        });
+        process.on("SIGINT", () => {
+            this.close();
+            process.exit();
+        });
     }
-  }
+
+    public getGuildMusicManager(guild: Guild): GuildMusicManager {
+        if (this.musicManagers.has(guild.id)) {
+            return this.musicManagers.get(guild.id);
+        }
+        const musicManager = new GuildMusicManager(guild);
+        this.musicManagers.set(guild.id, musicManager);
+        return musicManager;
+    }
+
+    public getGuildMusicManagerById(guildId: Snowflake): GuildMusicManager {
+        const guild = this.client.guilds.resolve(guildId);
+        if (guild) {
+            return this.getGuildMusicManager(guild);
+        } else {
+            throw new Error("Guild not available.");
+        }
+    }
+
+    public getGuildMusicManagerByIdIfExists(guildId: Snowflake): GuildMusicManager {
+        if (this.musicManagers.has(guildId)) {
+            return this.musicManagers.get(guildId);
+        } else {
+            throw new Error("Guild not available.");
+        }
+    }
+
+    private close() {
+        this.closeMusicManagers();
+        this.client.destroy();
+    }
+
+    private closeMusicManagers() {
+        for (const musicManager of this.musicManagers.values()) {
+            musicManager.close();
+        }
+    }
 }
