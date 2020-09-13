97247b05f58e4aef9d4fb603b000e2cb10cb95a7
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-09-13 09:37:25 +0200

    filter private videos in playlists

--- src/music/YoutubeService.ts
+++ src/music/YoutubeService.ts
@@ -6,153 +6,156 @@ import {PlaylistInfo, PlaylistItem, ShelfInfo, TrackInfo} from "./TrackInfo";
 
 export class YoutubeService {
 
-    public static currentId = 0;
+  public static currentId = 0;
 
-    public static getInstance() {
-        if (!this.instance) {
-            this.instance = new YoutubeService();
-        }
-        return this.instance;
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new YoutubeService();
     }
+    return this.instance;
+  }
 
-    public static resolveId(): number {
-        return YoutubeService.currentId++;
+  public static resolveId(): number {
+    return YoutubeService.currentId++;
+  }
+
+  private static instance: YoutubeService;
+
+  private static getInSeconds(duration: string): number {
+    if (duration) {
+      const split = duration.split(":");
+      const hours = split.length === 3 ? split[0] : "0";
+      const minutes = split.length === 3 ? split[1] : split[0];
+      const seconds = split.length === 3 ? split[2] : split[1];
+      return parseInt(hours) * 60 * 60 + parseInt(minutes) * 60 + parseInt(seconds);
+    } else {
+      return 0;
     }
+  }
 
-    private static instance: YoutubeService;
+  private static flatten(array: any[]): any[] {
+    return array.reduce((acc, val) => Array.isArray(val) ? acc.concat(YoutubeService.flatten(val)) : acc.concat(val), []);
+  }
 
-    private static getInSeconds(duration: string): number {
-        if (duration) {
-            const split = duration.split(":");
-            const hours = split.length === 3 ? split[0] : "0";
-            const minutes = split.length === 3 ? split[1] : split[0];
-            const seconds = split.length === 3 ? split[2] : split[1];
-            return parseInt(hours) * 60 * 60 + parseInt(minutes) * 60 + parseInt(seconds);
-        } else {
-            return 0;
-        }
+  private constructor() {
+  }
+
+  public getInfo(param: string): Promise<TrackInfo | TrackInfo[]> {
+    try {
+      if (ytpl.validateID(param)) {
+        return this.getPlaylistTracks(param);
+      } else if (ytdl.validateURL(param)) {
+        return this.getVideoInfo(param);
+      } else {
+        return this.searchVideoInfo(param);
+      }
+    } catch (e) {
+      throw new Error("No Video found.");
     }
+  }
 
-    private static flatten(array: any[]): any[] {
-        return array.reduce((acc, val) => Array.isArray(val) ? acc.concat(YoutubeService.flatten(val)) : acc.concat(val), []);
+  public getPlaylistTracks(param: string): Promise<TrackInfo[]> {
+    return ytpl(param)
+      .then(playlist => playlist.items.filter(item => item.author !== null && item.duration !== null))
+      .then(items => items.map(item => this.parsePlaylistItem(item)));
+  }
+
+  public getStream(url: string): Readable {
+    return ytdl(url, {filter: "audioonly", quality: "highestaudio", highWaterMark: 1 << 25});
+  }
+
+  public search(query: string): Promise<Array<TrackInfo | ShelfInfo | PlaylistInfo>> {
+    return ytsr(query, {
+      limit: 20
+    }).then(res => res.items.map(item => this.parse(item)).filter(video => video));
+  }
+
+  private parse(item: SearchItem): TrackInfo | ShelfInfo | PlaylistInfo {
+    if (item.type === "shelf-vertical") {
+      return this.parseShelfVertical(item as ShelfVertical);
+    } else if (item.type === "video") {
+      return this.parseVideo(item as Video);
+    } else if (item.type === "playlist") {
+      return this.parsePlaylist(item as Playlist);
     }
+    return undefined;
+  }
 
-    private constructor() {
-    }
+  private parseShelfVertical(shelf: ShelfVertical): ShelfInfo {
+    return {
+      type: "shelf",
+      title: shelf.title,
+      items: shelf.items.map(item => this.parseVideo(item)),
+      thumbnailUrl: shelf.items.length > 0 ? shelf.items[0].thumbnail : ""
+    };
+  }
 
-    public getInfo(param: string): Promise<TrackInfo | TrackInfo[]> {
-        try {
-            if (ytpl.validateID(param)) {
-                return this.getPlaylistTracks(param);
-            } else if (ytdl.validateURL(param)) {
-                return this.getVideoInfo(param);
-            } else {
-                return this.searchVideoInfo(param);
-            }
-        } catch (e) {
-            throw new Error("No Video found.");
-        }
-    }
+  private parseVideo(video: Video): TrackInfo {
+    return {
+      type: "video",
+      id: YoutubeService.currentId++,
+      url: video.link,
+      title: video.title,
+      artist: video.author.name,
+      thumbnailUrl: video.thumbnail,
+      duration: YoutubeService.getInSeconds(video.duration)
+    };
+  }
 
-    public getPlaylistTracks(param: string): Promise<TrackInfo[]> {
-        return ytpl(param).then(playlist => playlist.items.map(item => this.parsePlaylistItem(item)));
-    }
+  private parsePlaylist(playlist: Playlist): PlaylistInfo {
+    return {
+      type: "playlist",
+      title: playlist.title,
+      thumbnailUrl: playlist.thumbnail,
+      artist: playlist.author.name,
+      url: playlist.link,
+      length: playlist.length
+    };
+  }
 
-    public getStream(url: string): Readable {
-        return ytdl(url, {filter: "audioonly", quality: "highestaudio", highWaterMark: 1 << 25});
-    }
+  private searchVideoInfo(query: string): Promise<TrackInfo> {
+    return ytsr.getFilters(query).then(filters => {
+      const videoFilter = filters.get("Video");
+      return ytsr(null, {
+        limit: 4,
+        nextpageRef: videoFilter.ref
+      }).then(res => res.items.find(item => item.type === "video"))
+        .then(item => this.parseVideo(item as Video));
+    });
+  }
 
-    public search(query: string): Promise<Array<TrackInfo | ShelfInfo | PlaylistInfo>> {
-        return ytsr(query, {
-            limit: 20
-        }).then(res => res.items.map(item => this.parse(item)).filter(video => video));
-    }
-
-    private parse(item: SearchItem): TrackInfo | ShelfInfo | PlaylistInfo {
-        if (item.type === "shelf-vertical") {
-            return this.parseShelfVertical(item as ShelfVertical);
-        } else if (item.type === "video") {
-            return this.parseVideo(item as Video);
-        } else if (item.type === "playlist") {
-            return this.parsePlaylist(item as Playlist);
-        }
-        return undefined;
-    }
-
-    private parseShelfVertical(shelf: ShelfVertical): ShelfInfo {
+  private getVideoInfo(url: string): Promise<TrackInfo> {
+    return ytdl.getBasicInfo(url).then(video => {
+      if (video) {
+        const thumbnails = video.player_response.videoDetails.thumbnail.thumbnails;
         return {
-            type: "shelf",
-            title: shelf.title,
-            items: shelf.items.map(item => this.parseVideo(item)),
-            thumbnailUrl: shelf.items.length > 0 ? shelf.items[0].thumbnail : ""
+          type: "video",
+          id: YoutubeService.currentId++,
+          url: video.videoDetails.video_url,
+          title: video.videoDetails.title,
+          artist: video.videoDetails.author.name,
+          thumbnailUrl: thumbnails[thumbnails.length - 2].url,
+          duration: parseInt(video.player_response.videoDetails.lengthSeconds)
         };
-    }
+      } else {
+        throw new Error("Video not found");
+      }
+    });
+  }
 
-    private parseVideo(video: Video): TrackInfo {
-        return {
-            type: "video",
-            id: YoutubeService.currentId++,
-            url: video.link,
-            title: video.title,
-            artist: video.author.name,
-            thumbnailUrl: video.thumbnail,
-            duration: YoutubeService.getInSeconds(video.duration)
-        };
-    }
-
-    private parsePlaylist(playlist: Playlist): PlaylistInfo {
-        return {
-            type: "playlist",
-            title: playlist.title,
-            thumbnailUrl: playlist.thumbnail,
-            artist: playlist.author.name,
-            url: playlist.link,
-            length: playlist.length
-        };
-    }
-
-    private searchVideoInfo(query: string): Promise<TrackInfo> {
-        return ytsr.getFilters(query).then(filters => {
-            const videoFilter = filters.get("Video");
-            return ytsr(null, {
-                limit: 4,
-                nextpageRef: videoFilter.ref
-            }).then(res => res.items.find(item => item.type === "video"))
-                .then(item => this.parseVideo(item as Video));
-        });
-    }
-
-    private getVideoInfo(url: string): Promise<TrackInfo> {
-        return ytdl.getBasicInfo(url).then(video => {
-            if (video) {
-                const thumbnails = video.player_response.videoDetails.thumbnail.thumbnails;
-                return {
-                    type: "video",
-                    id: YoutubeService.currentId++,
-                    url: video.videoDetails.video_url,
-                    title: video.videoDetails.title,
-                    artist: video.videoDetails.author.name,
-                    thumbnailUrl: thumbnails[thumbnails.length - 2].url,
-                    duration: parseInt(video.player_response.videoDetails.lengthSeconds)
-                };
-            } else {
-                throw new Error("Video not found");
-            }
-        });
-    }
-
-    private parsePlaylistItem(item: PlaylistItem): TrackInfo {
-        return {
-            type: "video",
-            id: YoutubeService.currentId++,
-            url: item.url_simple,
-            title: item.title,
-            artist: item.author.name,
-            thumbnailUrl: item.thumbnail,
-            duration: YoutubeService.getInSeconds(item.duration)
-        };
-    }
+  private parsePlaylistItem(item: PlaylistItem): TrackInfo {
+    return {
+      type: "video",
+      id: YoutubeService.currentId++,
+      url: item.url_simple,
+      title: item.title,
+      artist: item.author.name,
+      thumbnailUrl: item.thumbnail,
+      duration: YoutubeService.getInSeconds(item.duration)
+    };
+  }
 }
+
 interface SearchItem {
-    type: string;
+  type: string;
 }
