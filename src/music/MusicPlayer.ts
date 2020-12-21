2d4e92cb6ebfe8bb2a67e2bb00c1eba3b8710911
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-12-21 23:29:36 +0100

    fix leave without music

--- src/music/MusicPlayer.ts
+++ src/music/MusicPlayer.ts
@@ -4,80 +4,84 @@ import {YoutubeService} from "./YoutubeService";
 
 export class MusicPlayer {
 
-    private get voiceConnection() {
-        if (this.guild.voice && this.guild.voice.connection) {
-            return this.guild.voice.connection;
-        }
-        throw new Error("Voice not connected. Add the Bot to a voice channel.");
+  private get voiceConnection() {
+    if (this.guild.voice && this.guild.voice.connection) {
+      return this.guild.voice.connection;
     }
+    throw new Error("Voice not connected. Add the Bot to a voice channel.");
+  }
 
-    private get dispatcher() {
-        if (this.voiceConnection.dispatcher) {
-            return this.voiceConnection.dispatcher;
-        }
-        throw new Error("There isn't any song playing");
+  private get dispatcher() {
+    if (this.voiceConnection.dispatcher) {
+      return this.voiceConnection.dispatcher;
     }
+    throw new Error("There isn't any song playing");
+  }
 
-    private observers: PlayerObserver[] = [];
+  private observers: PlayerObserver[] = [];
 
-    constructor(private guild: Guild) {
+  constructor(private guild: Guild) {
+  }
+
+  public register(observer: PlayerObserver) {
+    this.observers.push(observer);
+  }
+
+  public play(url: string) {
+    this.stop();
+    const stream = YoutubeService.getInstance().getStream(url);
+    const dispatcher = this.voiceConnection.play(stream, {highWaterMark: 1});
+    dispatcher.on("debug", (information: string) => this.forObservers(observer => observer.onDebug(information)));
+    dispatcher.on("start", () => this.forObservers(observer => observer.onStart()));
+    dispatcher.on("finish", () => this.forObservers(observer => observer.onEnd()));
+    dispatcher.on("error", (err: Error) => this.forObservers(observer => observer.onError(err)));
+    dispatcher.on("speaking", (value: boolean) => this.forObservers(observer => observer.onSpeaking(value)));
+  }
+
+  public pause() {
+    if (this.isCurrentlyPlaying()) {
+      this.dispatcher.pause();
     }
+    this.forObservers(observer => observer.onTogglePause(this.isPaused()));
+  }
 
-    public register(observer: PlayerObserver) {
-        this.observers.push(observer);
+  public resume() {
+    if (this.isCurrentlyPlaying()) {
+      this.dispatcher.resume();
     }
+    this.forObservers(observer => observer.onTogglePause(this.isPaused()));
+  }
 
-    public play(url: string) {
-        this.stop();
-        const stream = YoutubeService.getInstance().getStream(url);
-        const dispatcher = this.voiceConnection.play(stream, {highWaterMark: 1});
-        dispatcher.on("debug", (information: string) => this.forObservers(observer => observer.onDebug(information)));
-        dispatcher.on("start", () => this.forObservers(observer => observer.onStart()));
-        dispatcher.on("finish", () => this.forObservers(observer => observer.onEnd()));
-        dispatcher.on("error", (err: Error) => this.forObservers(observer => observer.onError(err)));
-        dispatcher.on("speaking", (value: boolean) => this.forObservers(observer => observer.onSpeaking(value)));
+  public isPaused(): boolean {
+    try {
+      return this.dispatcher.paused;
+    } catch (e) {
+      return true;
     }
+  }
 
-    public pause() {
-        if (this.isCurrentlyPlaying()) {
-            this.dispatcher.pause();
-        }
-        this.forObservers(observer => observer.onTogglePause(this.isPaused()));
-    }
+  public isCurrentlyPlaying() {
+    return this.isConnected() && this.guild.voice.connection.dispatcher && !this.dispatcher.writableEnded;
+  }
 
-    public resume() {
-        if (this.isCurrentlyPlaying()) {
-            this.dispatcher.resume();
-        }
-        this.forObservers(observer => observer.onTogglePause(this.isPaused()));
-    }
+  public isConnected() {
+    return this.guild && this.guild.voice && this.guild.voice.connection;
+  }
 
-    public isPaused(): boolean {
-        return this.dispatcher.paused;
-    }
+  public getPosition(): number {
+    return Math.floor(this.dispatcher.streamTime / 1000);
+  }
 
-    public isCurrentlyPlaying() {
-        return this.isConnected() && this.guild.voice.connection.dispatcher && !this.dispatcher.writableEnded;
+  public stop() {
+    if (this.isCurrentlyPlaying()) {
+      this.dispatcher.end();
+      this.dispatcher.removeAllListeners();
     }
+  }
 
-    public isConnected() {
-        return this.guild && this.guild.voice && this.guild.voice.connection;
+  private forObservers(func: (observer: PlayerObserver) => void) {
+    for (const observer of this.observers) {
+      func(observer);
     }
-
-    public getPosition(): number {
-        return Math.floor(this.dispatcher.streamTime / 1000);
-    }
-
-    public stop() {
-        if (this.isCurrentlyPlaying()) {
-            this.dispatcher.end();
-            this.dispatcher.removeAllListeners();
-        }
-    }
-
-    private forObservers(func: (observer: PlayerObserver) => void) {
-        for (const observer of this.observers) {
-            func(observer);
-        }
-    }
+  }
 }
