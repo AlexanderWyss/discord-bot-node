163d5464d536a9877fac42fdb433a6d5273acca9
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-03-25 16:52:18 +0100

    fix light mode in guild select

--- client/src/app/guild-select/guild-select.component.ts
+++ client/src/app/guild-select/guild-select.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GuildInfo} from '../models';
 import {MusicService} from '../music.service';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-guild-select',
@@ -8,12 +9,18 @@ import {MusicService} from '../music.service';
   styleUrls: ['./guild-select.component.scss']
 })
 export class GuildSelectComponent implements OnInit {
+  private readonly lightModeCookie = "light_mode";
   guilds: GuildInfo[] = [];
 
-  constructor(private musicService: MusicService) {
+  constructor(private musicService: MusicService, private cookieService: CookieService) {
   }
 
   ngOnInit(): void {
+    if (this.cookieService.check(this.lightModeCookie) && this.cookieService.get(this.lightModeCookie) !== "true") {
+      document.getElementById("body").classList.remove("lightTheme");
+    } else {
+      document.getElementById("body").classList.add("lightTheme");
+    }
     this.musicService.getGuilds().subscribe(guilds => this.guilds = guilds);
   }
 }
