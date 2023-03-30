d65e73332ab2aeb881f572871ce1285fff2592f8
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-03-30 20:05:04 +0200

    seek slash commands

--- src/commands/MusicControlCommands.ts
+++ src/commands/MusicControlCommands.ts
@@ -1,9 +1,19 @@
 import {Command} from "./Command";
 import {ChatInputCommandInteraction, GuildMember, SlashCommandBuilder} from "discord.js";
 import {Bot} from "../Bot";
+import {GuildMusicManager} from "../music/GuildMusicManager";
 
 function tryJoin(interaction: ChatInputCommandInteraction, bot: Bot) {
   bot.getGuildMusicManager(interaction.guild).tryJoin((interaction.member as GuildMember).voice.channel);
+}
+
+function seek(musicManager: GuildMusicManager, seconds: number) {
+  if (!musicManager.isVoiceConnected() || !musicManager.getCurrentTrack().url) {
+    return "Nothing currently playing.";
+  }
+  seconds = Math.min(Math.max(seconds, 0), musicManager.getCurrentTrack().duration);
+  musicManager.seek(seconds);
+  return `Jumped to ${seconds}s.`;
 }
 
 export const MUSIC_CONTROL_COMMANDS: Command[] = [
@@ -66,6 +76,36 @@ export const MUSIC_CONTROL_COMMANDS: Command[] = [
       const volume = interaction.options.getInteger("volume", true);
       bot.getGuildMusicManager(interaction.guild).setVolume(volume);
       return "Volume set to " + volume;
+    }
+  }, {
+    data: new SlashCommandBuilder()
+      .setName("seekfromstart")
+      .setDescription("Seek in the song.")
+      .addIntegerOption(option => option.setName("seconds")
+        .setDescription("Seconds from start.")
+        .setRequired(true)
+        .setMinValue(0)
+      ),
+    execute(interaction, bot) {
+      tryJoin(interaction, bot);
+      const seconds = interaction.options.getInteger("seconds", true);
+      const musicManager = bot.getGuildMusicManager(interaction.guild);
+      return seek(musicManager, seconds);
+    }
+  }, {
+    data: new SlashCommandBuilder()
+      .setName("seek")
+      .setDescription("Seek in the song relative to current position.")
+      .addIntegerOption(option => option.setName("seconds")
+        .setDescription("+/- seconds from current position.")
+        .setRequired(true)
+      ),
+    execute(interaction, bot) {
+      tryJoin(interaction, bot);
+      const seconds = interaction.options.getInteger("seconds", true);
+      const musicManager = bot.getGuildMusicManager(interaction.guild);
+      const positionAfter = musicManager.getCurrentTrack().position + seconds;
+      return seek(musicManager, positionAfter);
     }
   }, {
     data: new SlashCommandBuilder()
